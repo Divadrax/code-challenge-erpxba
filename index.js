04f7f618e9646a141f92915ebd0c93d93b05e6e3
David Scarratt <dgs@sail.com.au>
2020-11-09 07:52:02 +1100

    Add port configuration.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import test1 from './test1.js';
 import test2 from './test2.js';
 
+const port = process.env.RECKONPORT || 9999;
 const app = express();
 app.use('/', test1);
 app.use('/test2', test2);
-app.listen(9999);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
